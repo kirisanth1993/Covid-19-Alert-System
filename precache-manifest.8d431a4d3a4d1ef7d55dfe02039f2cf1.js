2640c4f0f8afc9cf0c7b0b0cb374f8834624cff9
kirisanth <kirisanth.14@cse.mrt.ac.lk>
2020-07-02 10:23:09 +0530

    Updates

---
+++ precache-manifest.8d431a4d3a4d1ef7d55dfe02039f2cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e0fe0e5029384a4485aeb3d8691610",
+    "url": "/Covid-19-Alert-System/index.html"
+  },
+  {
+    "revision": "0a164f32828aa230655a",
+    "url": "/Covid-19-Alert-System/static/css/main.631c3586.chunk.css"
+  },
+  {
+    "revision": "182f9748bea09ebe96d4",
+    "url": "/Covid-19-Alert-System/static/js/2.bb445bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid-19-Alert-System/static/js/2.bb445bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a164f32828aa230655a",
+    "url": "/Covid-19-Alert-System/static/js/main.5f856219.chunk.js"
+  },
+  {
+    "revision": "9d57412e2626ca026e04",
+    "url": "/Covid-19-Alert-System/static/js/runtime-main.7d46d3df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid-19-Alert-System/static/media/logo.5d5d9eef.svg"
+  }
+]);
